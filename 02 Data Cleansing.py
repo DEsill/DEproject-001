fba2f18ba44c78971816f1602f34679cbc416f1a
DEsill <aunji_engineer@hotmail.com>
2022-05-28 11:39:50 +0700

    DELETE.py FILE

--- 02 Data Cleansing.py
+++
@@ -1,6 +0,0 @@
-from pyspark.sql import SparkSession
-spark = SparkSession.builder.master("local[*]").getOrCreate()
-
-dt = spark.read.csv('ws2_data.csv', header = True, inferSchema = True, )
-
-spark
